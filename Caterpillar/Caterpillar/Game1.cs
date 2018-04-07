68d14cac7185a88bfb1041d4b12997afd94020a8
TheNightglow <Marcus.Streuber@gmx.de>
2018-04-07 18:04:24 +0200

    Bewegungstest in Game1
    ertestet wie Positionsänderung eines Modelles funktioniert, muss später aber noch richtig implementiert werden (nach Player-, Kisten-, und Raupenschwanzklasse)

--- Caterpillar/Caterpillar/Game1.cs
+++ Caterpillar/Caterpillar/Game1.cs
@@ -20,10 +20,20 @@ namespace Caterpillar
         //Momentanes Modell zum 3D zeug testen, Löschen wenn wir eine Player Klasse haben
         Model deleteMe;
         Model deleteMeTail;
+        Vector3 RotAroundYAxes = Vector3.Up;
+        //Matrix RotationMatrix;
+        //Quaternion rotationQuaternion = Quaternion.Identity;
+        Vector3 deleteMePlayerPosition = new Vector3(0f, 0f, 0f);
+        Vector3 deleteMePlayerTailPosition = new Vector3(0f, -0.45f, 0f);
+
+        //Konstanten
+        int viewSizeWidth = 1600; //Breite des Spielfensters
+        int viewSizeHeight = 900;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            //graphics.IsFullScreen = true;
             Content.RootDirectory = "Content";
         }
 
@@ -35,13 +45,16 @@ namespace Caterpillar
             camTarget = new Vector3(0f, 0f, 0f);
             camPosition = new Vector3(0f, 0f, -5);
             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
-                               MathHelper.ToRadians(45f), graphics.
-                               GraphicsDevice.Viewport.AspectRatio,
-                1f, 1000f);
+                               MathHelper.ToRadians(45f), 
+                               graphics.GraphicsDevice.Viewport.AspectRatio, 
+                               1f, 1000f);
             viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
-                         new Vector3(0f, 1f, 0f));// Y up
-            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.
-                          Forward, Vector3.Up);
+                         new Vector3(0f, 1f, 0f));
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);
+
+            graphics.PreferredBackBufferWidth = viewSizeWidth;  // Fenstermaße setzen
+            graphics.PreferredBackBufferHeight = viewSizeHeight;   
+            graphics.ApplyChanges();
 
             deleteMe = Content.Load<Model>("HeadV1");
             deleteMeTail = Content.Load<Model>("Tail1");
@@ -58,6 +71,8 @@ namespace Caterpillar
 
         protected override void Update(GameTime gameTime)
         {
+
+
             //Camera Drehen Test, später anderst implementieren
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
                 ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
@@ -95,16 +110,56 @@ namespace Caterpillar
                 camPosition.Z -= 0.1f;
             }
 
+            //Model Rotations Test
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                //RotationMatrix *= Matrix.CreateFromAxisAngle(RotationMatrix.Up, MathHelper.ToRadians(1.0f));
+                //rotationQuaternion.Y = MathHelper.ToRadians(-90);
+                RotAroundYAxes = Vector3.Up;
+                //worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, RotAroundYAxes);
+                deleteMePlayerPosition.Y += 0.1f;
+                deleteMePlayerTailPosition.Y += 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                RotAroundYAxes = Vector3.Right;
+                // worldMatrix = Matrix.CreateWorld(camTarget, camPosition, RotAroundYAxes);
+                deleteMePlayerPosition.X += 0.1f;
+                deleteMePlayerTailPosition.X += 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                RotAroundYAxes = Vector3.Down;
+                // worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, RotAroundYAxes);
+                deleteMePlayerPosition.Y -= 0.1f;
+                deleteMePlayerTailPosition.Y -= 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                RotAroundYAxes = Vector3.Left;
+                //  worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, RotAroundYAxes);
+                deleteMePlayerPosition.X -= 0.1f;
+                deleteMePlayerTailPosition.X -= 0.1f;
+            }
+
+            
+
+
             viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
                          Vector3.Up);
+
+           // worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, RotAroundYAxes);
             base.Update(gameTime);
+
+
         }
 
         protected override void Draw(GameTime gameTime)
         {
 
 
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.ForestGreen);
+           
 
             foreach (ModelMesh mesh in deleteMe.Meshes)
             {
@@ -113,7 +168,11 @@ namespace Caterpillar
                     //effect.EnableDefaultLighting();
                     effect.AmbientLightColor = new Vector3(1f, 0, 0);
                     effect.View = viewMatrix;
-                    effect.World = worldMatrix;
+                    effect.World = Matrix.CreateWorld(deleteMePlayerPosition, Vector3.Forward, RotAroundYAxes); ;
+                        /*transforms[mesh.ParentBone.Index] 
+         Matrix.CreateScale(1, 1, 1) *
+         Matrix.CreateFromQuaternion(rotationQuaternion) *
+         Matrix.CreateTranslation(camTarget);*/
                     effect.Projection = projectionMatrix;
                 }
                 mesh.Draw();
@@ -125,7 +184,7 @@ namespace Caterpillar
                     //effect.EnableDefaultLighting();
                     effect.AmbientLightColor = new Vector3(1f, 0, 0);
                     effect.View = viewMatrix;
-                    effect.World = Matrix.CreateWorld(new Vector3(0, -0.45f, 0), Vector3.Forward, Vector3.Up); ;
+                    effect.World = Matrix.CreateWorld(deleteMePlayerTailPosition, Vector3.Forward, Vector3.Up);
                     effect.Projection = projectionMatrix;
                 }
                 mesh.Draw();
